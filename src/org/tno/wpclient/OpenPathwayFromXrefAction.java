a0e3994151f042d73e77d6a053d06ad5795363bb
SravanthiSinha <sravani.sinha@gmail.com>
2013-06-22 19:06:02 +0530

    "Handled Exceptions in the case ,if the pathway is annotated with an invalid identifier ie identifer is nota number or is empty or datasource is empty "

--- src/org/tno/wpclient/OpenPathwayFromXrefAction.java
+++ src/org/tno/wpclient/OpenPathwayFromXrefAction.java
@@ -28,11 +28,26 @@ class OpenPathwayFromXrefAction extends AbstractAction {
 	
 	public void actionPerformed(ActionEvent evt) {
 		try {
+			int id=0;
 			Xref x = elm.getXref();
-			File tmpDir= new File(plugin.getTmpDir(), x.getDataSource().getFullName());
-			tmpDir.mkdirs();
-			plugin.openPathwayWithProgress(client, x.getId(), 0, tmpDir);
-		} catch (Exception e) {
+			try{
+				if(!(x.getDataSource().toString().isEmpty())){
+				id=Integer.parseInt(x.getId().toString()); // to check wheter the id is a NUMBER
+				}
+			
+			}catch (Exception e)
+			{
+			Logger.log.error("The Pathway was annotated with an invalid identifier", e);
+			 JOptionPane.showMessageDialog(null,"The Pathway is annotated with an invalid identifier", "ERROR", JOptionPane.ERROR_MESSAGE);
+			}
+			if(id>0)
+			{
+			 File tmpDir= new File(plugin.getTmpDir(), x.getDataSource().getFullName());
+			 tmpDir.mkdirs();
+			 plugin.openPathwayWithProgress(client, x.getId(), 0, tmpDir);					
+			} 
+		} catch (Exception e) 
+		{
 			Logger.log.error("Error while loading pathway from remote database", e);
 			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
 		}
