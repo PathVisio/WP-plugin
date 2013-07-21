d7a3bdc42685acf347487984e5b22c6ae850f377
Sravanthi Sinha <sravani.sinha@gmail.com>
2013-07-22 01:24:41 +0530

    Update LoginPanel.java

--- src/org/tno/wpclient/LoginPanel.java
+++ src/org/tno/wpclient/LoginPanel.java
@@ -1,63 +1,73 @@
 package org.tno.wpclient;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
+/**
+ * Class: LoginPane
+ * Description: A simple class to get user's loginname and password.
+ * NOTE: This class is not very secure!
+ * @author Le Cuong Nguyen
+ **/
+//package atnf.atoms.mon.util;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.BorderFactory;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-import org.pathvisio.core.debug.Logger;
+public class LoginPanel extends JPanel implements ActionListener {
+	String itsUsername = "";
+	String itsPassword = "";
+	boolean itsFirst = true;
+	boolean itsKeep = false;
+	JTextField itsUserField = new JTextField(15);
+	JPasswordField itsPassField = new JPasswordField(15);
+	JCheckBox itsKeepBox = new JCheckBox("Save details:", false);
+	boolean itsInit = false;
 
-import com.jgoodies.forms.layout.CellConstraints;
-import com.jgoodies.forms.layout.FormLayout;
+	public LoginPanel() {
+		super();
 
-public class LoginPanel extends JPanel{
+		setLayout(new GridLayout(3, 2));
+		add(new JLabel("Username:"));
+		add(itsUserField);
+		add(new JLabel("Password"));
+		add(itsPassField);
+		add(itsKeepBox);
+		JButton submit = new JButton("done");
+		add(submit);
+		submit.addActionListener(this);
 
-  private WikiPathwaysClientPlugin plugin;
-
-	public LoginPanel(WikiPathwaysClientPlugin plugin) {
-		this.plugin = plugin;
-
-		setLayout(new BorderLayout());
-
-		Action loginAction = new AbstractAction("Login")
-		{
-			private JTextField username;
-			private JTextField password;
-
-			public void actionPerformed(ActionEvent e)
-			{
-				try
-				{
-					
-				}
-				catch (Exception ex) 
-				{
-					JOptionPane.showMessageDialog(LoginPanel.this,ex.getMessage(), "Error",JOptionPane.ERROR_MESSAGE);
-					Logger.log.error("Error searching WikiPathways", ex);
-				}
-				
-				username = new JTextField();
-				password = new JTextField();
-				JPanel loginOptBox = new JPanel();
-				FormLayout layout = new FormLayout("p,3dlu,150px,3dlu,fill:pref:grow,3dlu,fill:pref:grow,3dlu","p, pref, p,  pref, p, 2dlu,2dlu");
-				CellConstraints cc = new CellConstraints();
-				loginOptBox.setLayout(layout);
-				loginOptBox.setBorder(BorderFactory.createTitledBorder(SearchPanel.etch,"UserCredentials"));
-				loginOptBox.add(new JLabel("Username"), cc.xy(1, 1));
-				loginOptBox.add(username, cc.xy(3, 1));
-				loginOptBox.add(new JLabel("Password"), cc.xy(1, 2));
-				loginOptBox.add(password, cc.xy(3, 2));
-				loginOptBox.add(new JButton("Submit"), cc.xy(4, 4));;
-				
-			}
-		};
 	}
 
+	public String[] getLogin() {
+		if (!itsKeep && !itsFirst) {
+			return null;
+		}
+		if (!itsInit) {
+			return null;
+		}
+		itsFirst = false;
+		String[] res = new String[2];
+		res[0] = itsUsername;
+		res[1] = itsPassword;
+		if (!itsKeep) {
+			itsUsername = "";
+			itsPassword = "";
+		}
+		return res;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		itsUsername = itsUserField.getText();
+		itsPassword = new String(itsPassField.getPassword());
+		itsKeep = itsKeepBox.isSelected();
+		itsInit = true;
+		setVisible(false);
+	}
 }
