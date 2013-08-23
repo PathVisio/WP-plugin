f9966d049da01114f73c6975584023e6d6b5904a
Sravanthi Sinha <sravani.sinha@gmail.com>
2013-08-23 08:32:15 +0530

    added update and create pathway functionality

--- src/org/pathvisio/wpclient/LoginPanel.java
+++ src/org/pathvisio/wpclient/LoginPanel.java
@@ -23,16 +23,30 @@ package org.pathvisio.wpclient;
  * @version 1.0
  **/
 
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
+import java.util.Collections;
+import java.util.Vector;
 
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+
+import org.pathvisio.core.debug.Logger;
+import org.pathvisio.core.model.Pathway;
+import org.pathvisio.desktop.PvDesktop;
+import org.pathvisio.wikipathways.webservice.WSPathway;
+import org.wikipathways.client.WikiPathwaysClient;
 
 public class LoginPanel extends JPanel implements ActionListener 
 {
@@ -44,11 +58,17 @@ public class LoginPanel extends JPanel implements ActionListener
 	JPasswordField itsPassField = new JPasswordField(15);
 	JCheckBox itsKeepBox = new JCheckBox("Save details:", false);
 	boolean itsInit = false;
+	private WikiPathwaysClientPlugin plugin;
+	private PvDesktop desktop;String clientName ;
+	private JComboBox clientDropdown; WikiPathwaysClient client ;
+	String actiontype;
 
-	public LoginPanel()
+	public LoginPanel(PvDesktop desktop, WikiPathwaysClientPlugin plugin, String actiontype)
 	{
 		super();
-
+		this.plugin=plugin;
+		this.desktop=desktop;
+		this.actiontype=actiontype;
 		setLayout(new GridLayout(3, 2));
 		add(new JLabel("Username:"));
 		add(itsUserField);
@@ -57,6 +77,19 @@ public class LoginPanel extends JPanel implements ActionListener
 		add(itsKeepBox);
 		JButton submit = new JButton("done");
 		add(submit);
+		Vector<String> clients = new Vector<String>(plugin.getClients().keySet());
+		Collections.sort(clients);
+		
+		clientDropdown = new JComboBox(clients);
+		clientDropdown.setSelectedIndex(0);
+		clientDropdown.setRenderer(new DefaultListCellRenderer() 
+		{
+			public Component getListCellRendererComponent(final JList list,final Object value, final int index,final boolean isSelected, final boolean cellHasFocus) 
+			{
+				String strValue = WikiPathwaysClientPlugin.shortClientName(value.toString());
+				return super.getListCellRendererComponent(list, strValue,index, isSelected, cellHasFocus);
+			}
+		});
 		submit.addActionListener(this);
 
 	}
@@ -84,10 +117,63 @@ public class LoginPanel extends JPanel implements ActionListener
 
 	public void actionPerformed(ActionEvent e)
 	{
-		itsUsername = itsUserField.getText();
-		itsPassword = new String(itsPassField.getPassword());
-		itsKeep = itsKeepBox.isSelected();
-		itsInit = true;
-		setVisible(false);
+	
+	
+
+		if(actiontype.equalsIgnoreCase("create"))
+			createPathway();
+		else if(actiontype.equalsIgnoreCase("update"))
+			updatePathway();
+		
 	}
+	
+	String user = "sravanthi";
+	String pass = "kmitkmit";
+	private void login() throws RemoteException {
+		 clientName = clientDropdown.getSelectedItem().toString();
+		 client = plugin.getClients().get(clientName);
+		client.login(user, pass);
+	
+	
+
+	}
+	public void createPathway(){
+		try {
+			login();
+		
+			
+			client.createPathway(desktop.getSwingEngine().getEngine().getActivePathway());
+			
+			JOptionPane.showMessageDialog(null,"The pathway created");
+		
+
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			
+		}
+	}
+	
+	public void updatePathway() {
+		try {
+			login();
+			WSPathway wsp = client.getPathway("WP1");
+			Pathway p = WikiPathwaysClient.toPathway(wsp);
+			p.getMappInfo().addComment("Soap test - " + System.currentTimeMillis(), "Soap test");
+
+			client.updatePathway(
+					"WP1", p,
+					"Soap test - " + System.currentTimeMillis(),
+					Integer.parseInt(wsp.getRevision()));
+			
+			
+			JOptionPane.showMessageDialog(null,"The pathway updated");
+
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			
+		}
+	}
+
 }
